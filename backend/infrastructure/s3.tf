ebd6811b0e7234d4f20811b6b295c1c1200e1031
Conor <csmyth95@gmail.com>
2025-05-23 14:23:42 +0000

    Use up to date S3 terraform resources

--- backend/infrastructure/s3.tf
+++ backend/infrastructure/s3.tf
@@ -1,23 +1,30 @@
 
 resource "aws_s3_bucket" "user_uploaded_documents" {
   bucket = "muinteoir-ai-user-uploaded-documents"
-  versioning {
-    enabled = true
-  }
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-  lifecycle_rule {
-    enabled = true
-    expiration {
-      days = 30
-    }
-  }
+
   tags = {
     ENV = var.env
+  }
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "user_document_lifecycle_config" {
+  bucket = aws_s3_bucket.bucket.user_uploaded_documents
+
+  rule {
+    id = "document_expiration"
+    expiration {
+        days = 30
+    }
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "user_document_encryption_config" {
+  bucket = aws_s3_bucket.mybucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "AES256"
+    }
   }
 }
