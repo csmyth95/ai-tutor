661e2e736c6982daca16b4bf386edabe9481696e
Conor <csmyth95@gmail.com>
2025-07-05 15:19:45 +0000

    Update vscode settings & reintroduce server.js

---
+++ backend/server/server.js
@@ -0,0 +1,31 @@
+import express, { json, urlencoded } from 'express';
+import cookieParser from 'cookie-parser';
+
+import db from './models/index.js';
+import userRoutes from './routes/user.js';
+import documentRoutes from './routes/document.js';
+
+//setting up your port
+const PORT = process.env.PORT || 4000;
+
+// Assign app variable to express.
+const app = express();
+
+//middleware
+app.use(json());
+app.use(urlencoded({ extended: true }));
+app.use(cookieParser());
+
+//synchronizing the database and forcing it to false so we dont lose data
+db.sequelize.sync({ force: true }).then(() => {
+  console.log("Sequilize has been re synced with db.")
+})
+
+//routes for the user API
+app.use('/api/v1/users', userRoutes)
+app.use('/api/v1/documents', documentRoutes);
+
+//listening to server connection
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
